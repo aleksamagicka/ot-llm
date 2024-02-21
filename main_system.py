d43248cacdf657c68c88c59cd809d1d121a5bc35
Aleksa Savić <savicaleksa83@gmail.com>
2024-02-21 14:46:32 +0100

    Add list of human_prompts; update system text
    Signed-off-by: Aleksa Savić <savicaleksa83@gmail.com>

--- main_system.py
+++ main_system.py
@@ -68,13 +68,15 @@ follows, enclosed in backticks:
   }
 }
 ```
-This schema is focused on artworks and includes various properties such as the artist, description, art form and author among others.
-There are other instances of this schema which you'll need to account for, as this isn't the only one, so don't use its values literally.
-If the prompt asks for a name, try to match the appropriate name (of author or artwork).
+This schema is focused on artworks and includes various properties such as the artist, description, art form and author, among others.
+There are other instances of this schema which you'll need to account for, as this isn't the only one, so don't use its values literally
+and try to match parts of the prompt to additionalValues if possible.
+If the prompt asks for a name, try to match the appropriate name (of author or artwork). If the prompt contains keywords, try to match
+them also to additional properties.
 
 Instructions:
 Use only the node types and properties provided in the schema.
-Do not use any node types and properties that are not explicitly provided.
+Do not use any node types and properties that are not explicitly provided. Use only the provided VisualArtwork schema.
 Include all necessary prefixes.
 Note: Be as concise as possible.
 Do not include any explanations or apologies in your responses.
@@ -85,10 +87,23 @@ The question is:
 {prompt}"""
 
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-messages = [
-    SystemMessage(content=SPARQL_GENERATION_SELECT_TEMPLATE),
-    HumanMessage(content="Give me all artworks whose name contains 'aleksa'")
-]
-response = chat(messages)
 
-print(response.content, end='\n')
+human_prompts = [
+    "Give me all artworks whose name contains 'aleksa'",
+    "Give me all artworks where author is 'aleksa'",
+    "Give me all artworks related to anime",
+]
+
+for prompt in human_prompts:
+    print(f"{prompt}:\n")
+    print("```")
+
+    messages = [
+        SystemMessage(content=SPARQL_GENERATION_SELECT_TEMPLATE),
+        HumanMessage(content=prompt)
+    ]
+    response = chat(messages)
+
+    print(response.content, end='\n')
+    print("```")
+    print("------------------------")
